97075aecc5dfd0cd603132b9690b58e767593ca2
Juan Matthys Uys <opyate@gmail.com>
2014-09-30 13:24:19 +0100

    use postgresql for testing

--- environment_test.sh
+++ environment_test.sh
@@ -1,1 +1,8 @@
-export DATABASE_URL='sqlite://'
+# Here we'll create a test database, and override the database to the test values.
+export APP_NAME="matching_test"
+set +o errexit
+createuser -s $APP_NAME
+createdb -U $APP_NAME -O $APP_NAME $APP_NAME -T template0
+set -e
+
+export DATABASE_URL="postgresql://localhost/$APP_NAME"
